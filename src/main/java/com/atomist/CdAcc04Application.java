ed86f0eb2a8664fab7ebab5b560189f8879b9534
Christian Dupuis <cd@atomist.com>
2019-06-03 09:07:47 +0200

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/atomist/CdAcc04Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdAcc04Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(CdAcc04Application.class, args);
+	}
+
+}
